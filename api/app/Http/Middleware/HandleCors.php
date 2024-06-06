a5a8766cba86cad6578354475f90d16db7f24486
Dave Njoroge <d.njoroge@thecodingmachine.com>
2024-06-06 15:35:02 +0300

    initial commit

---
+++ api/app/Http/Middleware/HandleCors.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HandleCors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response  = $next($request);
+
+        $allowedOrigins = config('cors.allowed_origins');
+
+        $requestOrigin = $request->header('Origin');
+
+        if (in_array($requestOrigin, $allowedOrigins)) {
+            $response->headers->set('Access-Control-Allow-Origin', env('FRONTEND_URL','http://localhost:3000'));
+            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+            $response->headers->set('Access-Control-Allow-Credentials', 'true');
+            return $response;
+        }
+ 
+        return response()->json(['error' => 'Not allowed by CORS policy'], 403);
+
+    }
+}
