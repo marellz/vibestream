839c746f60c1a4a857d755a1fa93d038d4e5ba73
Dave Njoroge <d.njoroge@thecodingmachine.com>
2024-06-06 17:39:02 +0300

    global store

---
+++ front/store/global.ts
@@ -0,0 +1,13 @@
+import { defineStore } from "pinia";
+export const useGlobalStore = defineStore("global", () => {
+  const { $api } = useNuxtApp();
+  const version = ref<string|null>(null);
+  const getVersion = async () => {
+    const response : { version: string} = await $api.get("http://localhost:8000/api");
+    version.value = response.version
+  };
+
+  return {
+    version, getVersion
+  }
+});
