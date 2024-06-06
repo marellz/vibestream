1571c00297f36dd331837303ede3d435d253f412
Dave Njoroge <d.njoroge@thecodingmachine.com>
2024-06-06 17:02:34 +0300

    installed axios, tw and added config

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  css: ["~/assets/style/app.css"],
+  runtimeConfig: {
+    public: {
+      API_URL: process.env.API_URL,
+    },
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
