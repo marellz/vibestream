b1e17a9dd9395dfc2c90e9c6ec86ef73f0684687
Dave Njoroge <d.njoroge@thecodingmachine.com>
2024-06-07 14:30:25 +0300

    controller response method

--- api/app/Http/Controllers/Controller.php
+++ api/app/Http/Controllers/Controller.php
@@ -4,9 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    //
+    public function respond($data): JsonResponse
+    {
+        return response()->json($data);
+    }
 }
