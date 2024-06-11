e1227a40bfb0d6eb9a33e898597d5817fb4f686c
Dave Njoroge <d.njoroge@thecodingmachine.com>
2024-06-11 12:49:36 +0300

    tabs, alert component

---
+++ front/components/x/alert.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="rounded border p-2 mb-10"
+  :class="variants[variant]">
+    <p>
+      <slot />
+    </p>
+  </div>
+</template>
+<script lang="ts" setup>
+withDefaults(
+  defineProps<{
+    variant: "info" | "error";
+  }>(),
+  {
+    variant: "info",
+  }
+);
+
+const variants = {
+  error: "border-red-500 bg-red-50 text-red-500",
+  info: "border-blue-500 bg-blue-50 text-blue-500",
+  success: "border-green-500 bg-green-50 text-green-500",
+};
+</script>
---
+++ front/components/x/tabs.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <ul class="flex items-baseline border-b border-b-gray-200">
+      <li
+        v-for="tab in tabs"
+        :key="`tab-${tab.key}`"
+        class="px-3 first:pl-0 border-b -mb-px py-4"
+        :class="{ 'border-b-gray-900 font-bold': activeTab === tab.key }"
+      >
+        <a :href="`#${tab.key}`" @click="switchTab(tab.key)">
+          {{ tab.label }}
+        </a>
+      </li>
+    </ul>
+    <div class="relative">
+      <x-content>
+        <slot :name="activeTab" />
+      </x-content>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+interface TabItem {
+  key: string;
+  label: string;
+}
+
+const route = useRoute();
+const props = defineProps<{
+  tabs: TabItem[];
+  defaultTab?: string;
+}>();
+
+onMounted(() => {
+
+  if (props.defaultTab && props.tabs.find((t) => t.key === props.defaultTab)) {
+    activeTab.value = props.defaultTab;
+  }
+
+  if(route.hash) {
+    let tabKey = route.hash.slice(1)
+    if ( props.tabs.find((t) => t.key === tabKey)) {
+      activeTab.value = tabKey;
+    }
+  }
+});
+
+const activeTab = ref<string>(props.tabs[0].key);
+
+const switchTab = (key: string) => {
+  activeTab.value = key;
+};
+</script>
