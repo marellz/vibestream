24d4d15fdb039afe0f22b66b5c829a415011adf0
Dave Njoroge <d.njoroge@thecodingmachine.com>
2024-06-18 17:17:28 +0300

    update status route

--- front/store/global.ts
+++ front/store/global.ts
@@ -3,7 +3,7 @@ export const useGlobalStore = defineStore("global", () => {
   const { $api } = useNuxtApp();
   const version = ref<string|null>(null);
   const getVersion = async () => {
-    const response : { version: string} = await $api.get("http://localhost:8000/api");
+    const response : { version: string} = await $api.get("http://localhost:8000/api/status");
     version.value = response.version
   };
 
