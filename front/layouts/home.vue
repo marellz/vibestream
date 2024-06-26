043e50b31643f692dd1f2224c0a1e64d8ea80508
Dave Njoroge <d.njoroge@thecodingmachine.com>
2024-06-26 12:47:09 +0300

    home update with auth

--- front/layouts/home.vue
+++ front/layouts/home.vue
@@ -7,3 +7,13 @@
     <layouts-footer />
   </div>
 </template>
+<script lang="ts" setup>
+import { useAuthStore } from '~/store/auth';
+
+const auth = useAuthStore()
+
+onBeforeMount(() => {
+  auth.initialize();
+});
+
+</script>
