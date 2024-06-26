d84b9f6869253b6686897c8cceaa9919aa5da5ef
Dave Njoroge <d.njoroge@thecodingmachine.com>
2024-06-26 12:42:40 +0300

    update layouts - auth initialize

--- front/layouts/auth.vue
+++ front/layouts/auth.vue
@@ -11,4 +11,13 @@
     <layouts-footer />
   </div>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useAuthStore } from '~/store/auth';
+
+const auth = useAuthStore()
+
+onBeforeMount(() => {
+  auth.initialize();
+});
+
+</script>
---
+++ front/layouts/home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div ref="wrapper" id="wrapper" class="flex flex-col h-[100vh]">
+    <layouts-header />
+    <main class="flex-auto">
+        <slot />
+    </main>
+    <layouts-footer />
+  </div>
+</template>
